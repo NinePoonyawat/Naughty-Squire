8a4916f657c4fd456f89f32ebc3112887efffbf8
Poonyawat Suntiniwat <ninekrub139@gmail.com>
2022-08-29 22:15:49 +0700

    add sensitivity

--- Assets/Scripts/PlayerControl/ThirdPersonShooterController.cs
+++ Assets/Scripts/PlayerControl/ThirdPersonShooterController.cs
@@ -9,11 +9,15 @@ namespace Player
     public class ThirdPersonShooterController : MonoBehaviour
     {
         [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
+        [SerializeField] private float normalSensitivity;
+        [SerializeField] private float aimSensitivity;
 
+        private ThirdPersonController thirdPersonController;
         private StarterAssetsInputs starterAssetInputs;
 
         private void Awake()
         {
+            thirdPersonController = GetComponent<ThirdPersonController>();
             starterAssetInputs = GetComponent<StarterAssetsInputs>();
         }
         // Update is called once per frame
@@ -22,10 +26,12 @@ namespace Player
             if (starterAssetInputs.aim)
             {
                 aimVirtualCamera.gameObject.SetActive(true);
+                thirdPersonController.SetSensitivity(aimSensitivity);
             }
             else
             {
                 aimVirtualCamera.gameObject.SetActive(false);
+                thirdPersonController.SetSensitivity(normalSensitivity);
             }
         }
     }
