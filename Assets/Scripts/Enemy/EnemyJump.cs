e83e0a4e68361768a29c727663ddd399e5b43cf7
Itar <natdanaitrintawat@gmail.com>
2022-09-23 22:53:47 +0700

    Better Jump cooldown

--- Assets/Scripts/Enemy/EnemyJump.cs
+++ Assets/Scripts/Enemy/EnemyJump.cs
@@ -18,6 +18,7 @@ public class EnemyJump : EnemyBase
        distance = 4;
        timeBetweenAttacks = 2f;
        EnemyState = State.Idle;
+       //Physics.IgnoreCollision(player.GetComponent<Collider>(), GetComponent<Collider>());
     }
 
     public override void walking() {
@@ -25,10 +26,16 @@ public class EnemyJump : EnemyBase
         if (EnemyState != State.Attack) {
             agent.SetDestination(player.transform.position);
             CheckJumping();
+            StopCoroutine("Jump");
+            timeBetweenAttacks -= Time.deltaTime;
         } else {
             Debug.Log("NOT yet");
             //Invoke("Jump",timeBetweenAttacks);
             StartCoroutine("Jump");
+            // if (NavMesh.SamplePosition(player.transform.position, out NavMeshHit hit, 1f, agent.areaMask)) {
+            // Debug.Log("WARP");
+            // agent.Warp(hit.position);
+            // }
         }
     }
 
@@ -41,35 +48,56 @@ public class EnemyJump : EnemyBase
     }
 
     public void CheckJumping() {
-        if (playerIsInLOS && Vector3.Distance(transform.position,player.transform.position) <= distance) EnemyState = State.Attack;
+        if (playerIsInLOS && Vector3.Distance(transform.position,player.transform.position) <= distance && timeBetweenAttacks < 0) EnemyState = State.Attack;
     }
+
+    // void OnCollisionEnter(Collision collision)
+    // {
+    //   if (collision.gameObject.tag == "Player")
+    //   {
+    //     Debug.Log("COLLIDE IGNORE");
+    //     Physics.IgnoreCollision(player.GetComponent<Collider>(), GetComponent<Collider>());
+    //   }
+    // }
 
     public IEnumerator Jump() {
         Debug.Log("JUMP");
         Vector3 startingPosition = transform.position;
+        Vector3 endingPosition = player.transform.position - transform.forward;
+        
 
         for (float time =0; time < 1; time += Time.deltaTime * JumpSpeed) {
-            transform.position = Vector3.Lerp(startingPosition, player.transform.position, time) + Vector3.up * HeightCurve.Evaluate(time);
+            transform.position = Vector3.Lerp(startingPosition, endingPosition, time) + Vector3.up * HeightCurve.Evaluate(time);
             transform.rotation = Quaternion.Slerp(transform.rotation, 
-                Quaternion.LookRotation(player.transform.position - transform.position),time);
+                Quaternion.LookRotation(endingPosition - transform.position),time);
 
             yield return null;
         }
-        Collider[] colliders = Physics.OverlapSphere(transform.position,radius);
-        foreach (Collider hit in colliders) {
-            //Debug.Log(hit.tag);
-            if (hit.tag == "Player") {
-                Debug.Log("BOMB");
-                Rigidbody rb = hit.GetComponent<Rigidbody>();
-                if (rb != null) {
-                    rb.AddExplosionForce(power,transform.position,radius);
-                }
-            }
-        }
+        // Collider[] colliders = Physics.OverlapSphere(transform.position,radius);
+        // foreach (Collider hit in colliders) {
+        //     //Debug.Log(hit.tag);
+        //     if (hit.tag == "Player") {
+        //         // Debug.Log("BOMB");
+        //         // Rigidbody rb = hit.GetComponent<Rigidbody>();
+        //         // if (rb != null) {
+        //         //     rb.AddExplosionForce(power,transform.position,radius);
+        //         // }
+        //         // Vector3 hitDirection = hit.transform.position - transform.position;
+        //         // hitDirection = hitDirection.normalized;
+        //         // Debug.Log(hitDirection);
+        //         //Quaternion angle = hit.transform.rotation;
+        //         //Debug.Log(angle);
+        //         //Debug.Log(player.transform.rotation);
+        //         //Vector3 knockbackPosition = angle.eulerAngles.normalized;//new Vector3(angle.x,angle.y,angle.z);
+        //         //Debug.Log(knockbackPosition);
+        //         //hit.transform.position += knockbackPosition * -10;
+        //     }
+        // }
         // if (NavMesh.SamplePosition(player.transform.position, out NavMeshHit hit, 1f, agent.areaMask)) {
-        //     Debug.Log("WARP");
-        //     agent.Warp(hit.position);
+        //      Debug.Log("WARP");
+        //      agent.Warp(hit.position);
         // }
         EnemyState = State.Idle;
+        timeBetweenAttacks = 2f;
     }
 }
