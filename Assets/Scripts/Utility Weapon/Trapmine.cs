765a7a5408332a2eab4f07735c2b3e25eb3f6ba5
Nithimet Smart <89740267+KiraraGZ@users.noreply.github.com>
2022-10-11 14:00:56 +0700

    add Trapmine

--- Assets/Scripts/Utility Weapon/Trapmine.cs
+++ Assets/Scripts/Utility Weapon/Trapmine.cs
@@ -4,13 +4,48 @@ using UnityEngine;
 
 public class Trapmine : MonoBehaviour
 {
+    [SerializeField] private float detectRadius;
+    [SerializeField] private float explodeRadius;
+
+    [SerializeField] private GameObject explodeEffect;
+
+    [SerializeField] private float damage;
+
+    public Color meshColor = Color.red;
+
     void Update()
     {
-        
+        Collider[] colliders = Physics.OverlapSphere(transform.position, detectRadius);
+
+        foreach (Collider nearbyObject in colliders)
+        {
+            if (nearbyObject.CompareTag("Enemy"))
+            {
+                Debug.Log("BOOM");
+                Explode();
+            }
+        }
     }
 
     private void Explode()
     {
+        Instantiate(explodeEffect, this.transform.position, Quaternion.identity);
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explodeRadius);
+
+        foreach (Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            Debug.Log(rb);
+        }
+
         Destroy(this.gameObject);
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = meshColor;
+        Gizmos.DrawSphere(transform.position, detectRadius);
+        Gizmos.DrawSphere(transform.position, explodeRadius);
     }
 }
