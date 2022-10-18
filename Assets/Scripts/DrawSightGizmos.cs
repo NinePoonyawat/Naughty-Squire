47eb7e2d0f88a65af6daab06a7d8a5be9fd8b53f
Itar <natdanaitrintawat@gmail.com>
2022-10-18 17:57:51 +0700

    new sight script

---
+++ Assets/Scripts/DrawSightGizmos.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawSightGizmos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public EnemyBase enemy;
+    public Color meshColor = Color.red;
+    Mesh mesh;
+
+    void Start()
+    {
+        meshColor.a = 0.5f;
+    }
+
+    // Update is called once per frame
+    void OnValidate() {
+        mesh = CreateWedgeMesh();
+    }
+
+    void OnDrawGizmos() {
+        if (mesh) {
+            Gizmos.color = meshColor;
+            //transform.Rotate(enemy.transform.forward.x,enemy.transform.forward.y,enemy.transform.forward.z);
+            Gizmos.DrawMesh(mesh,transform.position,transform.rotation);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position, enemy.agent.destination);
+        }
+    }
+
+    Mesh CreateWedgeMesh() {
+        Mesh mesh = new Mesh();
+
+        float fieldOfViewAngle = enemy.fieldOfViewAngle;
+        float losRadius = enemy.losRadius;
+        float height = enemy.height;
+
+        int segments = 10;
+        int numTriangles = (segments *4) + 2 + 2;
+        int numVertices = numTriangles * 3;
+
+        Vector3[] vertices = new Vector3[numVertices];
+        int[] triangles = new int[numVertices];
+
+        Vector3 bottomCenter = Vector3.zero;
+        Vector3 bottomLeft = Quaternion.Euler(0, -fieldOfViewAngle, 0) * Vector3.forward*losRadius;
+        Vector3 bottomRight = Quaternion.Euler(0, fieldOfViewAngle, 0) * Vector3.forward*losRadius;
+
+        Vector3 topCenter = bottomCenter + Vector3.up*height;
+        Vector3 topRight = bottomRight + Vector3.up*height;
+        Vector3 topLeft = bottomLeft + Vector3.up*height;
+
+        int vert = 0;
+
+        //left side
+        vertices[vert++]  = bottomCenter;
+        vertices[vert++] = bottomLeft;
+        vertices[vert++] = topLeft;
+
+        vertices[vert++] = topLeft;
+        vertices[vert++]  = topCenter;
+        vertices[vert++] = bottomCenter;
+        //right side
+        vertices[vert++]  = bottomCenter;
+        vertices[vert++] = topCenter;
+        vertices[vert++] = topRight;
+
+        vertices[vert++] = topRight;
+        vertices[vert++]  = bottomRight;
+        vertices[vert++] = bottomCenter;
+        
+        float currentAngle = -fieldOfViewAngle;
+        float deltaAngle = (fieldOfViewAngle *2) / segments;
+        for (int i =0; i<segments; ++i) {
+            bottomLeft = Quaternion.Euler(0, currentAngle, 0) * Vector3.forward*losRadius;
+            bottomRight = Quaternion.Euler(0, currentAngle + deltaAngle, 0) * Vector3.forward*losRadius;
+
+            topCenter = bottomCenter + Vector3.up*height;
+            topRight = bottomRight + Vector3.up*height;
+            topLeft = bottomLeft + Vector3.up*height;
+
+            //far side 
+            vertices[vert++]  = bottomLeft;
+            vertices[vert++] = bottomRight;
+            vertices[vert++] = topRight;
+
+            vertices[vert++] = topRight;
+            vertices[vert++]  = topLeft;
+            vertices[vert++] = bottomLeft;
+
+            //top
+            vertices[vert++] = topCenter;
+            vertices[vert++]  = topLeft;
+            vertices[vert++] = topRight;
+
+            //bottom
+            vertices[vert++] = bottomCenter;
+            vertices[vert++]  = bottomRight;
+            vertices[vert++] = bottomLeft;    
+            
+            currentAngle += deltaAngle;
+        }
+        
+
+        for (int i =0; i< numVertices; i++) {
+            triangles[i] = i;
+        }
+        mesh.vertices = vertices;
+        mesh.triangles  = triangles;
+        mesh.RecalculateNormals();
+
+        return mesh;
+    }
+
+}
--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -32,7 +32,7 @@ public abstract class EnemyBase : MonoBehaviour
     public float losRadius;
     public float height = 0.1f;
     public float StopDistance;
-    public Color meshColor = Color.red;
+    //public Color meshColor = Color.red;
 
 
     //ai sight and memory
@@ -54,6 +54,7 @@ public abstract class EnemyBase : MonoBehaviour
     protected float timeTilAlert = 10f;
     protected float timeTilNextMovement = 2f;
     protected float timeBetweenAttacks = 3.5f;
+    public bool FleeAble;
 
 
     private StarterAssetsInputs starterAssetInputs;
@@ -63,7 +64,7 @@ public abstract class EnemyBase : MonoBehaviour
     
     void Start() {
         EnemyState = State.Idle;
-        meshColor.a = 0.5f;
+        //meshColor.a = 0.5f;
         //starterAssetInputs = GetComponent<StarterAssetsInputs>();
         if(group == -1) group = Random.Range(0,3);
         //AIManager.Instance.Units.Add(this);
@@ -99,7 +100,7 @@ public abstract class EnemyBase : MonoBehaviour
         //Debug.Log(player.GetComponent<Collider>().tag);
         //Debug.Log(EnemyState);
         CheckLOS();
-        CheckFlee();
+        if (FleeAble) CheckFlee();
         StartNextState();
         NoiseCheck();
         
@@ -290,98 +291,98 @@ public abstract class EnemyBase : MonoBehaviour
         aiMemoriesPlayer = false;
         EnemyState = State.Idle;
     }
-    void OnValidate() {
-        mesh = CreateWedgeMesh();
-    }
+    // void OnValidate() {
+    //     mesh = CreateWedgeMesh();
+    // }
 
-    void OnDrawGizmos() {
-        if (mesh) {
-            Gizmos.color = meshColor;
-            Gizmos.DrawMesh(mesh,transform.position,transform.rotation);
-            Gizmos.color = Color.blue;
-            Gizmos.DrawLine(transform.position, agent.destination);
-        }
-    }
+    // void OnDrawGizmos() {
+    //     if (mesh) {
+    //         Gizmos.color = meshColor;
+    //         Gizmos.DrawMesh(mesh,transform.position,transform.rotation);
+    //         Gizmos.color = Color.blue;
+    //         Gizmos.DrawLine(transform.position, agent.destination);
+    //     }
+    // }
 
-    Mesh CreateWedgeMesh() {
-        Mesh mesh = new Mesh();
+    // Mesh CreateWedgeMesh() {
+    //     Mesh mesh = new Mesh();
 
-        int segments = 10;
-        int numTriangles = (segments *4) + 2 + 2;
-        int numVertices = numTriangles * 3;
+    //     int segments = 10;
+    //     int numTriangles = (segments *4) + 2 + 2;
+    //     int numVertices = numTriangles * 3;
 
-        Vector3[] vertices = new Vector3[numVertices];
-        int[] triangles = new int[numVertices];
+    //     Vector3[] vertices = new Vector3[numVertices];
+    //     int[] triangles = new int[numVertices];
 
-        Vector3 bottomCenter = Vector3.zero;
-        Vector3 bottomLeft = Quaternion.Euler(0, -fieldOfViewAngle, 0) * Vector3.forward*losRadius;
-        Vector3 bottomRight = Quaternion.Euler(0, fieldOfViewAngle, 0) * Vector3.forward*losRadius;
+    //     Vector3 bottomCenter = Vector3.zero;
+    //     Vector3 bottomLeft = Quaternion.Euler(0, -fieldOfViewAngle, 0) * Vector3.forward*losRadius;
+    //     Vector3 bottomRight = Quaternion.Euler(0, fieldOfViewAngle, 0) * Vector3.forward*losRadius;
 
-        Vector3 topCenter = bottomCenter + Vector3.up*height;
-        Vector3 topRight = bottomRight + Vector3.up*height;
-        Vector3 topLeft = bottomLeft + Vector3.up*height;
+    //     Vector3 topCenter = bottomCenter + Vector3.up*height;
+    //     Vector3 topRight = bottomRight + Vector3.up*height;
+    //     Vector3 topLeft = bottomLeft + Vector3.up*height;
 
-        int vert = 0;
+    //     int vert = 0;
 
-        //left side
-        vertices[vert++]  = bottomCenter;
-        vertices[vert++] = bottomLeft;
-        vertices[vert++] = topLeft;
+    //     //left side
+    //     vertices[vert++]  = bottomCenter;
+    //     vertices[vert++] = bottomLeft;
+    //     vertices[vert++] = topLeft;
 
-        vertices[vert++] = topLeft;
-        vertices[vert++]  = topCenter;
-        vertices[vert++] = bottomCenter;
-        //right side
-        vertices[vert++]  = bottomCenter;
-        vertices[vert++] = topCenter;
-        vertices[vert++] = topRight;
+    //     vertices[vert++] = topLeft;
+    //     vertices[vert++]  = topCenter;
+    //     vertices[vert++] = bottomCenter;
+    //     //right side
+    //     vertices[vert++]  = bottomCenter;
+    //     vertices[vert++] = topCenter;
+    //     vertices[vert++] = topRight;
 
-        vertices[vert++] = topRight;
-        vertices[vert++]  = bottomRight;
-        vertices[vert++] = bottomCenter;
+    //     vertices[vert++] = topRight;
+    //     vertices[vert++]  = bottomRight;
+    //     vertices[vert++] = bottomCenter;
         
-        float currentAngle = -fieldOfViewAngle;
-        float deltaAngle = (fieldOfViewAngle *2) / segments;
-        for (int i =0; i<segments; ++i) {
-            bottomLeft = Quaternion.Euler(0, currentAngle, 0) * Vector3.forward*losRadius;
-            bottomRight = Quaternion.Euler(0, currentAngle + deltaAngle, 0) * Vector3.forward*losRadius;
+    //     float currentAngle = -fieldOfViewAngle;
+    //     float deltaAngle = (fieldOfViewAngle *2) / segments;
+    //     for (int i =0; i<segments; ++i) {
+    //         bottomLeft = Quaternion.Euler(0, currentAngle, 0) * Vector3.forward*losRadius;
+    //         bottomRight = Quaternion.Euler(0, currentAngle + deltaAngle, 0) * Vector3.forward*losRadius;
 
-            topCenter = bottomCenter + Vector3.up*height;
-            topRight = bottomRight + Vector3.up*height;
-            topLeft = bottomLeft + Vector3.up*height;
+    //         topCenter = bottomCenter + Vector3.up*height;
+    //         topRight = bottomRight + Vector3.up*height;
+    //         topLeft = bottomLeft + Vector3.up*height;
 
-            //far side 
-            vertices[vert++]  = bottomLeft;
-            vertices[vert++] = bottomRight;
-            vertices[vert++] = topRight;
+    //         //far side 
+    //         vertices[vert++]  = bottomLeft;
+    //         vertices[vert++] = bottomRight;
+    //         vertices[vert++] = topRight;
 
-            vertices[vert++] = topRight;
-            vertices[vert++]  = topLeft;
-            vertices[vert++] = bottomLeft;
+    //         vertices[vert++] = topRight;
+    //         vertices[vert++]  = topLeft;
+    //         vertices[vert++] = bottomLeft;
 
-            //top
-            vertices[vert++] = topCenter;
-            vertices[vert++]  = topLeft;
-            vertices[vert++] = topRight;
+    //         //top
+    //         vertices[vert++] = topCenter;
+    //         vertices[vert++]  = topLeft;
+    //         vertices[vert++] = topRight;
 
-            //bottom
-            vertices[vert++] = bottomCenter;
-            vertices[vert++]  = bottomRight;
-            vertices[vert++] = bottomLeft;    
+    //         //bottom
+    //         vertices[vert++] = bottomCenter;
+    //         vertices[vert++]  = bottomRight;
+    //         vertices[vert++] = bottomLeft;    
             
-            currentAngle += deltaAngle;
-        }
+    //         currentAngle += deltaAngle;
+    //     }
         
 
-        for (int i =0; i< numVertices; i++) {
-            triangles[i] = i;
-        }
-        mesh.vertices = vertices;
-        mesh.triangles  = triangles;
-        mesh.RecalculateNormals();
+    //     for (int i =0; i< numVertices; i++) {
+    //         triangles[i] = i;
+    //     }
+    //     mesh.vertices = vertices;
+    //     mesh.triangles  = triangles;
+    //     mesh.RecalculateNormals();
 
-        return mesh;
-    }
+    //     return mesh;
+    // }
 
 
 
