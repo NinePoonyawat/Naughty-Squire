cb70b8b31e2b5521a22b574acb861e6053552dc2
Poonyawat Suntiniwat <ninekrub139@gmail.com>
2022-11-14 21:17:21 +0700

    set ammo text

--- Assets/Scripts/Gun/GunSystem.cs
+++ Assets/Scripts/Gun/GunSystem.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using StarterAssets;
 using System;
+using TMPro;
 
 namespace Weapon
 {
     public class GunSystem : MonoBehaviour
     {
-        [SerializeField] private int bulletPerMagazine;
+        [SerializeField] private int bulletPerMagazine = 0;
         [SerializeField] private float damage;
-        private int bulletLeftInMagazine;
+        private int bulletLeftInMagazine = 0;
         private bool isOutOfAmmo;
 
         private StarterAssetsInputs starterAssetsInputs;
@@ -25,6 +26,8 @@ namespace Weapon
         private bool isCooldown = false;
         private string fireSoundName;
         private string reloadSoundName;
+
+        [SerializeField] private TMP_Text ammoText;
 
         public event EventHandler OnOutOfAmmoEvent;
         public event EventHandler OnReloadEvent;
@@ -53,6 +56,8 @@ namespace Weapon
             inventoryManager.OnInventoryClose += closeInventory;
 
             if (currentData == null) isArmed = false;
+
+            SetAmmoText(bulletLeftInMagazine,bulletPerMagazine);
         }
 
         void Update()
@@ -78,12 +83,12 @@ namespace Weapon
             cooldownTimeCount = cooldownTime;
             isCooldown = true;
             bulletLeftInMagazine--;
+            SetAmmoText(bulletLeftInMagazine,bulletPerMagazine);
             if (bulletLeftInMagazine <= 0)
             {
                 OnOutOfAmmoEvent?.Invoke(this,EventArgs.Empty);
                 SetOutOfAmmo(true);
             }
-
             FindObjectOfType<AudioManager>().Play(fireSoundName);
         }
 
@@ -94,7 +99,7 @@ namespace Weapon
             starterAssetsInputs.reload = false;
             if (!isArmed) return;
             bulletLeftInMagazine = bulletPerMagazine;
-            Debug.Log(bulletLeftInMagazine);
+            SetAmmoText(bulletLeftInMagazine,bulletPerMagazine);
             SetOutOfAmmo(false);
 
             isCooldown = true;
@@ -114,6 +119,7 @@ namespace Weapon
             fireSoundName = weaponData.fireSoundName;
             reloadSoundName = weaponData.reloadSoundName;
             if (bulletLeftInMagazine > 0) SetOutOfAmmo(false);
+            SetAmmoText(bulletLeftInMagazine,bulletPerMagazine);
             //Reload();
         }
 
@@ -129,12 +135,17 @@ namespace Weapon
 
         public void disarm()
         {
-            Debug.Log("enter");
             isArmed = false;
             currentData = null;
             damage = 0;
             bulletPerMagazine = 0;
             bulletLeftInMagazine = 0;
+            SetAmmoText(bulletLeftInMagazine,bulletPerMagazine);
+        }
+
+        public void SetAmmoText(int ammoRemain,int maxAmmo)
+        {
+            ammoText.SetText(ammoRemain + " / " + maxAmmo);
         }
 
         void SetOutOfAmmo(bool newInput)
